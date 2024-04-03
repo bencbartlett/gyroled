04f9c9c15e1e063d7b7f916e28500b68b32fda56
Ben Bartlett <benjamincbartlett@gmail.com>
2024-04-02 23:32:25 -0700

    working version of sound reactivity

--- src/main.cpp
+++ src/main.cpp
@@ -1,5 +1,6 @@
 #include <Arduino.h>
 #include <arduinoFFT.h>
+// #include <FastLED.h>
 #include <Adafruit_NeoPixel.h>
 // #include <Servo.h>
 #include <ESP32Servo.h>
@@ -21,15 +22,30 @@
 #define BRIGHTNESS 100
 
 
-#define SAMPLES         512           // CAN"T CHANGE AT THE MOMENT, Must be a power of 2
-#define SAMPLING_FREQ   40000         // Hz, must be 40000 or less due to ADC conversion time. Determines maximum frequency that can be analysed by the FFT Fmax=sampleF/2.
+#define SAMPLES         64           // CAN"T CHANGE AT THE MOMENT, Must be a power of 2
+#define SAMPLING_FREQ   20000         // Hz, must be 40000 or less due to ADC conversion time. Determines maximum frequency that can be analysed by the FFT Fmax=sampleF/2.
 #define AMPLITUDE       1000          // Depending on your audio source level, you may need to alter this value. Can be used as a 'sensitivity' control.
 #define NUM_BANDS       8             // To change this, you will need to change the bunch of if statements describing the mapping from bins to bands
-#define NOISE           100           // Used as a crude noise filter, values below this are ignored
+#define NOISE           20           // Used as a crude noise filter, values below this are ignored
 // #define TOP             16
 #define USE_AVERAGING   false
 #define USE_ROLLING_AMPLITUDE_AVERAGING false
+#define PEAK_DECAY_RATE 1
+#define NOISE_EMA_ALPHA 0.02        // At about 25fps this is about a 2 second window
 
+float avgNoise = 10.0;
+
+float servo_master_speed = 0.3; // Can range from 0 to 1
+float servo1_speed = 0.7; // Can range from -1 to 1
+float servo2_speed = 0.8;
+float servo3_speed = 1.0;
+
+int ring_1_midpoint_L = (int) (LED_COUNT_RING_1 * 0.25);
+int ring_1_midpoint_R = (int) (LED_COUNT_RING_1 * 0.75 + 1); // fudge factor
+int ring_2_midpoint_L = (int) (LED_COUNT_RING_2 * 0.25 + LED_COUNT_RING_1);
+int ring_2_midpoint_R = (int) (LED_COUNT_RING_2 * 0.75 + LED_COUNT_RING_1 - 1); // fudge factor
+int ring_3_midpoint_L = (int) (LED_COUNT_RING_3 * 0.25 + LED_COUNT_RING_2 + LED_COUNT_RING_1);
+int ring_3_midpoint_R = (int) (LED_COUNT_RING_3 * 0.75 + LED_COUNT_RING_2 + LED_COUNT_RING_1);
 
 // Sampling and FFT stuff
 unsigned int sampling_period_us;
@@ -41,7 +57,8 @@ int oldBarHeights[SAMPLES];
 int bandValues[SAMPLES];
 float vReal[SAMPLES];
 float vImag[SAMPLES];
-int noiseLevel;
+int noiseLevel = 0;
+int frame = 0;
 unsigned long newTime;
 
 float updatesPerSecond = 0.0;
@@ -76,7 +93,7 @@ unsigned long lastUpdate = 0;
 
 void setup() {
 
-  Serial.begin(115200);
+  Serial.begin(9600);
 
   strip.begin();
   strip.clear();
@@ -95,8 +112,9 @@ void setup() {
   servo2.attach(SERVO_2_PIN, 500, 2400);
   servo3.attach(SERVO_3_PIN, 500, 2400);
 
-  sampling_period_us = round(1000000 * (1.0 / SAMPLING_FREQ));
 
+  sampling_period_us = round(1000000 * (1.0 / SAMPLING_FREQ));
+  
   // Reset bandValues[]
   for (int i = 0; i<NUM_BANDS; i++){
     bandValues[i] = 0;
@@ -142,59 +160,51 @@ void whiteOverRainbow() {
   }
 }
 
+int clip255(int value) {
+    // Ensure the value is not less than minVal and not greater than maxVal
+    return std::min(std::max(value, 0), 255);
+}
 
-
-void rainbowFade2White(int wait, int rainbowLoops, int whiteLoops) {
+void loopyRainbow() {
   int fadeVal=0, fadeMax=100;
   fadeVal = 100;
-
-  // Hue of first pixel runs 'rainbowLoops' complete loops through the color
-  // wheel. Color wheel has a range of 65536 but it's OK if we roll over, so
-  // just count from 0 to rainbowLoops*65536, using steps of 256 so we
-  // advance around the wheel at a decent clip.
-  for(uint32_t firstPixelHue = 0; firstPixelHue < rainbowLoops*65536; firstPixelHue += 256) {
 
     for(int i=0; i<strip.numPixels(); i++) { // For each pixel in strip...
 
       // Offset pixel hue by an amount to make one full revolution of the
       // color wheel (range of 65536) along the length of the strip
       // (strip.numPixels() steps):
-      uint32_t pixelHue = firstPixelHue + (i * 65536L / strip.numPixels());
+      uint32_t pixelHue = frame * 1000 + (i * 65536L / strip.numPixels());
 
       // strip.ColorHSV() can take 1 or 3 arguments: a hue (0 to 65535) or
       // optionally add saturation and value (brightness) (each 0 to 255).
       // Here we're using just the three-argument variant, though the
       // second value (saturation) is a constant 255.
-      strip.setPixelColor(i, strip.gamma32(strip.ColorHSV(pixelHue, 255,
-        255 * fadeVal / fadeMax)));
+      uint32_t rgb = strip.gamma32(strip.ColorHSV(pixelHue, 255, 255 * fadeVal / fadeMax));
+      // Extract RGB values from the RGB color
+      // uint8_t r = (uint8_t)(rgb >> 16), g = (uint8_t)(rgb >> 8), b = (uint8_t)rgb;
+      // uint8_t w = clip255(noiseLevel * 3);
+      // strip.setPixelColor(i, r, g, b, w);
+      strip.setPixelColor(i, strip.gamma32(strip.ColorHSV(pixelHue, 255, 255 * fadeVal / fadeMax)));
     }
+}
 
-    // strip.show();
-    // delay(wait);
-
-    // if(firstPixelHue < 65536) {                              // First loop,
-    //   if(fadeVal < fadeMax) fadeVal++;                       // fade in
-    // } else if(firstPixelHue >= ((rainbowLoops-1) * 65536)) { // Last loop,
-    //   if(fadeVal > 0) fadeVal--;                             // fade out
-    // } else {
-    //   fadeVal = fadeMax; // Interim loop, make sure fade is at max
-    // }
+void applyWhiteBars(int noiseLevel) {
+  int factor = 0.95;
+  int amp = noiseLevel - avgNoise * factor > 0 ? (noiseLevel - avgNoise * factor) * 1 : 0;
+  if (amp > 0)  {
+    strip.fill(strip.Color(255, 255, 255, 255), ring_1_midpoint_L - amp, 2 * amp);
+    strip.fill(strip.Color(255, 255, 255, 255), ring_1_midpoint_R - amp, 2 * amp);
+    strip.fill(strip.Color(255, 255, 255, 255), ring_2_midpoint_L - amp, 2 * amp);
+    strip.fill(strip.Color(255, 255, 255, 255), ring_2_midpoint_R - amp, 2 * amp);
+    strip.fill(strip.Color(255, 255, 255, 255), ring_3_midpoint_L - amp, 2 * amp);
+    strip.fill(strip.Color(255, 255, 255, 255), ring_3_midpoint_R - amp, 2 * amp);
   }
+  
 
-  // for(int k=0; k<whiteLoops; k++) {
-  //   for(int j=0; j<256; j++) { // Ramp up 0 to 255
-  //     // Fill entire strip with white at gamma-corrected brightness level 'j':
-  //     strip.fill(strip.Color(0, 0, 0, strip.gamma8(j)));
-  //     strip.show();
-  //   }
-  //   delay(1000); // Pause 1 second
-  //   for(int j=255; j>=0; j--) { // Ramp down 255 to 0
-  //     strip.fill(strip.Color(0, 0, 0, strip.gamma8(j)));
-  //     strip.show();
-  //   }
+  // for(int i=0; i<noiseLevel; i++) { 
+  //   strip.setPixelColor(i, strip.Color(255, 255, 255, 255)); 
   // }
-
-  // delay(500); // Pause 1/2 second
 }
 
 
@@ -227,17 +237,18 @@ void colorWipe(uint32_t color, int wait) {
 
 void loop() {
 
-  String output = "";
+  frame++;
 
   updatesPerSecond = 1000.0 / float(millis() - lastUpdate);
   lastUpdate = millis();
 
-  output += "Updates per second: ";
+  String output = "";
+  // output += "Updates per second: ";
   output += updatesPerSecond;
   output += "\n";
 
   // if (millis() - lastSample > 10) {
-  if (true) {
+  #if true
     lastSample = millis();
     // Reset bandValues[]
     for (int i = 0; i<NUM_BANDS; i++){
@@ -262,15 +273,24 @@ void loop() {
     for (int i = 2; i < (SAMPLES/2); i++){       // Don't use sample 0 and only first SAMPLES/2 are usable. Each array element represents a frequency bin and its value the amplitude.
       if (vReal[i] > NOISE) {                    // Add a crude noise filter
 
-        // 8 bands, 12kHz top band
-        if (i<=3 )                bandValues[0]  += (int)vReal[i];
-        else if (i>3   && i<=6  ) bandValues[1]  += (int)vReal[i];
-        else if (i>6   && i<=13 ) bandValues[2]  += (int)vReal[i];
-        else if (i>13  && i<=27 ) bandValues[3]  += (int)vReal[i];
-        else if (i>27  && i<=55 ) bandValues[4]  += (int)vReal[i];
-        else if (i>55  && i<=112) bandValues[5]  += (int)vReal[i];
-        else if (i>112 && i<=229) bandValues[6]  += (int)vReal[i];
-        else if (i>229          ) bandValues[7]  += (int)vReal[i];
+        // // 8 bands, 12kHz top band
+        // if (i<=3 )                bandValues[0]  += (int)vReal[i];
+        // else if (i>3   && i<=6  ) bandValues[1]  += (int)vReal[i];
+        // else if (i>6   && i<=13 ) bandValues[2]  += (int)vReal[i];
+        // else if (i>13  && i<=27 ) bandValues[3]  += (int)vReal[i];
+        // else if (i>27  && i<=55 ) bandValues[4]  += (int)vReal[i];
+        // else if (i>55  && i<=112) bandValues[5]  += (int)vReal[i];
+        // else if (i>112 && i<=229) bandValues[6]  += (int)vReal[i];
+        // else if (i>229          ) bandValues[7]  += (int)vReal[i];
+
+        if (i<=bandLimits[0])                         bandValues[0]  += (int)vReal[i];
+        else if (i>bandLimits[0] && i<=bandLimits[1]) bandValues[1]  += (int)vReal[i];
+        else if (i>bandLimits[1] && i<=bandLimits[2]) bandValues[2]  += (int)vReal[i];
+        else if (i>bandLimits[2] && i<=bandLimits[3]) bandValues[3]  += (int)vReal[i];
+        else if (i>bandLimits[3] && i<=bandLimits[4]) bandValues[4]  += (int)vReal[i];
+        else if (i>bandLimits[4] && i<=bandLimits[5]) bandValues[5]  += (int)vReal[i];
+        else if (i>bandLimits[5] && i<=bandLimits[6]) bandValues[6]  += (int)vReal[i];
+        else if (i>bandLimits[6])                     bandValues[7]  += (int)vReal[i];
 
         // //16 bands, 12kHz top band
         //   if (i<=2 )           bandValues[0]  += (int)vReal[i];
@@ -315,40 +335,45 @@ void loop() {
       oldBarHeights[band] = barHeight;
     }
 
-    // Decay peak
-    if (millis() - lastPeakUpdate > 20) {
-      lastPeakUpdate = millis();
-      for (byte band = 0; band < NUM_BANDS; band++) {
-        if (peak[band] > 0) peak[band] -= 1;
-      }
+    // // Decay peak
+    // if (millis() - lastPeakUpdate > 20) {
+    //   lastPeakUpdate = millis();
+
+    // }
+
+    for (byte band = 0; band < NUM_BANDS; band++) {
+      if (peak[band] > 0) peak[band] -= PEAK_DECAY_RATE;
     }
 
-    
+    noiseLevel = (peak[0] + peak[1] + peak[2] + peak[3]);
+    avgNoise = (NOISE_EMA_ALPHA * noiseLevel) + ((1 - NOISE_EMA_ALPHA) * avgNoise);
+    // noiseLevel = (oldBarHeights[1] + oldBarHeights[2] + oldBarHeights[3] + oldBarHeights[4]); // / (AMPLITUDE / 4);
+
+  #else
+    noiseLevel = frame % 2 == 0 ? 0 : 100;
+  #endif
 
 
-  }
 
-  noiseLevel = peak[1] + peak[2] + peak[3] + peak[4] / AMPLITUDE;
+
+
+
   output += "noiseLevel: ";
   output += noiseLevel;
   output += "\n";
 
 
+  // output += "Spectrogram: \n";
+  // for (int band = 0; band < NUM_BANDS; band++) { 
+  //   output += band;
+  //   for(int i = 0; i < oldBarHeights[band]; i++) {
+  //     output += ("=");
+  //   }
+  //   output += "\n";
+  // }
 
+  // output += "\n\n";
 
-
-
-  output += "Spectrogram: \n";
-  for (int band = 0; band < NUM_BANDS; band++) { 
-    output += band;
-    for(int i = 0; i < oldBarHeights[band]; i++) {
-      output += ("=");
-    }
-    output += "\n";
-  }
-
-  output += "\n\n";
-  // Serial.println(noiseLevel);
 
   // =====================================================================
 
@@ -357,11 +382,11 @@ void loop() {
 
   if (updateServo) {
     // Example servo movements
-    servo1.write(0);
-    servo2.write(0);
-    servo3.write(0);
+    servo1.write((int) (90 + 90 * (servo_master_speed * servo1_speed)));
+    servo2.write((int) (90 + 90 * (servo_master_speed * servo2_speed)));
+    servo3.write((int) (90 + 90 * (servo_master_speed * servo3_speed)));
     updateServo = false; // Prevent updating the servo in the next loop iteration
-    // Serial.println("update servo");
+    Serial.println((int) (90 + 90 * (servo_master_speed * servo1_speed)) );
   }
 
   // Simple non-blocking delay for servo update
@@ -372,13 +397,11 @@ void loop() {
 
   // pulseWhiteAndRGB();
 
-  whiteOverRainbow(); // Update LEDs
+  // whiteOverRainbow(); // Update LEDs
 
-  // rainbowFade2White(3, 3, 1);
+  loopyRainbow();
 
-  for(int i=0; i<noiseLevel; i++) { 
-    strip.setPixelColor(i, strip.Color(255, 255, 255, 255)); 
-  }
+  applyWhiteBars(noiseLevel);
 
   strip.show();
 
