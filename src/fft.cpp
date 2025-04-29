b81ad7df640e69ed4e60dc6f965e85fe73eedece
Ben Bartlett <benbartlett@openai.com>
2025-04-28 19:57:54 -0700

    remove unused i2s sampling scheme

--- src/fft.cpp
+++ src/fft.cpp
@@ -33,15 +33,6 @@
   #define USE_RAW_ADC_READ false
 #endif
 
-// Alternate INMP441 sampling scheme
-#define USE_INMP441 		false	// Whether to use the INMP441 microphone or the default MAX4466
-#define I2S_SAMPLE_RATE     44100   // Sample rate of the INMP441 mic
-#define I2S_SAMPLE_SIZE     32      // Bits per sample
-#define I2S_WS  			12      // aka LRCL
-#define I2S_SD  			33      // aka DOUT
-#define I2S_SCK 			13      // aka BCLK
-#define I2S_NUM         	I2S_NUM_0
-
 // WLED FFT params
 static float fftResultPink[NUM_GEQ_CHANNELS] = { 1.70f, 1.71f, 1.73f, 1.78f, 1.68f, 1.56f, 1.55f, 1.63f, 1.79f, 1.62f, 1.80f, 2.06f, 2.47f, 3.35f, 6.83f, 9.55f }; // Table of multiplication factors so that we can even out the frequency response.
 static uint8_t FFTScalingMode = 3;            // 0 none; 1 optimized logarithmic; 2 optimized linear; 3 optimized square root
@@ -129,35 +120,6 @@ float currentBandEnergy[NUM_BANDS];
 float lastBandEnergy[NUM_BANDS];
 
 // unsigned long newTime;
-
-
-void setupI2S() {
-	// The I2S config as per the example
-	const i2s_config_t i2s_config = {
-		.mode = i2s_mode_t(I2S_MODE_MASTER | I2S_MODE_RX), // Receive, not transfer
-		.sample_rate = I2S_SAMPLE_RATE,
-		.bits_per_sample = I2S_BITS_PER_SAMPLE_32BIT,
-		.channel_format = I2S_CHANNEL_FMT_ONLY_LEFT,
-		.communication_format = (i2s_comm_format_t)(I2S_COMM_FORMAT_STAND_I2S),
-		.intr_alloc_flags = ESP_INTR_FLAG_LEVEL1,
-		.dma_buf_count = 8,
-		.dma_buf_len = SAMPLES,
-		.use_apll = false,
-		.tx_desc_auto_clear = false,
-		.fixed_mclk = 0,
-	};
-	// The pin config as per the setup
-	const i2s_pin_config_t pin_config = {
-		.bck_io_num = I2S_SCK,
-		.ws_io_num = I2S_WS,
-		.data_out_num = -1,
-		.data_in_num = I2S_SD
-	};
-	// Installing the I2S driver
-	i2s_driver_install(I2S_NUM, &i2s_config, 0, NULL);
-	i2s_set_pin(I2S_NUM, &pin_config);
-}
-
 
 static void async_sampling(void* arg) {
 # if USE_RAW_ADC_READ
